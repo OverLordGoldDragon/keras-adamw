cc719d9e3bc27be618d17504668dceb5211771be
OverLordGoldDragon <OverLordGoldDragon@yahoo.com>
2019-10-16 19:14:19 -0400

    Test .sh syntax

--- test.sh
+++ test.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-pycodestyle --max-line-length=89 keras_adamw tests && \
-    nosetests --nocapture --with-coverage --cover-erase --cover-html --cover-html-dir=htmlcov --ignore-files="example.py" \
-    --cover-package=keras_adamw --with-doctest tests
+pycodestyle --max-line-length=89 keras_adamw tests
+
+VAR="1"
+
+if [[ "$VAR" == "1" ]]; then
+    nosetests \
+        --nocapture --with-coverage --cover-erase --cover-html --cover-html-dir=htmlcov --ignore-files="example.py" \
+        --cover-package=keras_adamw --with-doctest tests
+fi
