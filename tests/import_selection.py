fc7eff038e4314f96a0142051d25310169dfea46
OverLordGoldDragon <OverLordGoldDragon@yahoo.com>
2020-02-03 07:53:46 +0400

    v1.15: Automate imports, more
    **New features**:
    
     - Simplified imports; just use `from keras_adamw import` for both the optimizers and utils, only requiring `import os; os.environ["TF_KERAS"]='1'` if using `tensorflow.keras` imports.
    
    **Breaking changes**:
    
     - Environment flags are now set via `'1'` and `'0'`, instead of `'True'` and `'False'`
    
    **Bug fixes**:
     
     - `get_configs()` incorrectly cast `eta_min`, `eta_max`, and `eta_t` via `int` - changed to `float`

--- tests/import_selection.py
+++ tests/import_selection.py
@@ -1,39 +1,17 @@
 import os
 
 
-tf_version = float(os.environ["TF_VERSION"][:3])
-tf_keras = bool(os.environ["TF_KERAS"] == "True")
-tf_python = bool(os.environ["TF_PYTHON"] == "True")
+TF_KERAS = bool(os.environ.get("TF_KERAS", '0') == '1')
+TF_2 = bool(os.environ.get("TF_VERSION", '1')[0] == '2')
 
 
-if tf_version >= 2:
-    if tf_keras:
-        from keras_adamw.optimizers_v2 import AdamW, NadamW, SGDW
-    elif tf_python:
-        from keras_adamw.optimizers_tfpy import AdamW, NadamW, SGDW
-    else:
-        from keras_adamw.optimizers import AdamW, NadamW, SGDW
-else:
-    if tf_keras:
-        from keras_adamw.optimizers_225tf import AdamW, NadamW, SGDW
-    else:
-        from keras_adamw.optimizers_225 import AdamW, NadamW, SGDW
-
-if tf_keras:
+if TF_KERAS:
     import tensorflow.keras.backend as K
     from tensorflow.keras.layers import Input, Dense, GRU, Bidirectional, Embedding
     from tensorflow.keras.models import Model, load_model
     from tensorflow.keras.regularizers import l2
     from tensorflow.keras.constraints import MaxNorm as maxnorm
     from tensorflow.keras.optimizers import Adam, Nadam, SGD
-elif tf_python:
-    import tensorflow.keras.backend as K  # tf.python.keras.backend is very buggy
-    from tensorflow.python.keras.layers import Input, Dense, GRU, Bidirectional
-    from tensorflow.python.keras.layers import Embedding
-    from tensorflow.python.keras.models import Model, load_model
-    from tensorflow.python.keras.regularizers import l2
-    from tensorflow.python.keras.constraints import MaxNorm as maxnorm
-    from tensorflow.python.keras.optimizers import Adam, Nadam, SGD
 else:
     import keras.backend as K
     from keras.layers import Input, Dense, GRU, Bidirectional, Embedding
@@ -42,7 +20,7 @@ else:
     from keras.constraints import MaxNorm as maxnorm
     from keras.optimizers import Adam, Nadam, SGD
 
-if tf_version < 2 and tf_keras:
+if (not TF_2) and TF_KERAS:
     from keras_adamw.utils_225tf import get_weight_decays, fill_dict_in_order
     from keras_adamw.utils_225tf import reset_seeds, K_eval
 else:
@@ -50,11 +28,10 @@ else:
     from keras_adamw.utils import reset_seeds, K_eval
 
 
-# ALL TESTS (7 total):
+# ALL TESTS (6 total):
 #  - keras    (TF 1.14.0, Keras 2.2.5) [test_optimizers.py]
 #  - tf.keras (TF 1.14.0, Keras 2.2.5) [test_optimizers_v2.py]
 #  - keras    (TF 2.0.0,  Keras 2.3.0) [test_optimizers.py     --TF_EAGER=True]
 #  - keras    (TF 2.0.0,  Keras 2.3.0) [test_optimizers.py     --TF_EAGER=False]
 #  - tf.keras (TF 2.0.0,  Keras 2.3.0) [test_optimizers_v2.py, --TF_EAGER=True]
 #  - tf.keras (TF 2.0.0,  Keras 2.3.0) [test_optimizers_v2.py, --TF_EAGER=False]
-#  - tf.python.keras (TF 2.0.0,  Keras 2.3.0) [test_optimizers_tfpy.py]
